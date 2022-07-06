158fbb82be93aa2cf526bb04df1120d6bc396729
Natalia Teplynina <nt@mnt9.com>
2022-07-06 16:58:06 +0300

    layout signup form

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.utils.translation import gettext
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from .forms import SignUpForm
 
 
 # def index(request):
@@ -11,11 +15,15 @@ class HomeView(TemplateView):
     template_name = "index.html"
 
 
-class UserCreateView(TemplateView):
+class UserCreateView(FormView):
+    
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
     template_name = "register.html"
 
-    def post(self, *args, **kwargs):
-        pass
+    def form_valid(self, form):
+        form.save()
+        return super(UserCreateView, self).form_valid(form)
 
 
 class LoginView(TemplateView):
