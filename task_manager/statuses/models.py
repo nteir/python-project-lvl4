434c1b41deece9f390d592db593013b757a5b378
Natalia Teplynina <nt@mnt9.com>
2022-07-11 16:23:37 +0300

    make status names unique

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class TaskStatus(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     date_created = models.DateTimeField(auto_now=True)
