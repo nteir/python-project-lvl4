93347a20448bd8477fa143394760897359bb3a68
Natalia Teplynina <nt@mnt9.com>
2022-07-12 16:21:19 +0300

    fix tests for new user model

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import TaskStatus
 from django.urls import reverse
+
+User = get_user_model()
 
 
 # Create your tests here.
--- task_manager/tests.py
+++ task_manager/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.urls import reverse
+
+User = get_user_model()
 
 
 class UsersTestCase(TestCase):
