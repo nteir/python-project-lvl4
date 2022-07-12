dc7f01544e6c3ec21cbc8b645115ed04122bff8d
Natalia Teplynina <nt@mnt9.com>
2022-07-12 16:24:49 +0300

    fix code style

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,7 +5,7 @@ class User(AbstractUser):
 
     class Meta:
         db_table = 'auth_user'
-    
+
     def __str__(self) -> str:
         if self.first_name and self.last_name:
             return f'{self.first_name} {self.last_name}'
