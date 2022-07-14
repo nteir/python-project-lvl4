15a003b7a28074a07097f0e764f8e554280b6e48
Natalia Teplynina <nt@mnt9.com>
2022-07-14 08:55:01 +0300

    add translations

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -6,10 +6,20 @@ from django.utils.translation import gettext as _
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(_('Name'), max_length=100, blank=False, unique=True)
-    description = models.TextField(_('Description'))
-    status = models.ForeignKey(TaskStatus, on_delete=models.PROTECT)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='created')
-    executor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='assigned')
-    date_created = models.DateTimeField(_('Date Created'), auto_now_add=True)
-    date_modified = models.DateTimeField(_('Date Modified'), auto_now=True)
+    name = models.CharField(max_length=100, blank=False, unique=True, verbose_name=_('Name'))
+    description = models.TextField(verbose_name=_('Description'))
+    status = models.ForeignKey(TaskStatus, on_delete=models.PROTECT, verbose_name=_('Status'))
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='created',
+        verbose_name=_('Author')
+    )
+    executor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='assigned',
+        verbose_name=_('Executor')
+    )
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name=_('Date Created'))
+    date_modified = models.DateTimeField(auto_now=True, verbose_name=_('Date Modified'))
