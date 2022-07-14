5d9b1322e9b6093eb80dbb07081536d3e349769d
Natalia Teplynina <nt@mnt9.com>
2022-07-14 15:45:32 +0300

    add tests for labels

--- task_manager/labels/tests.py
+++ task_manager/labels/tests.py
@@ -1,3 +1,40 @@
-# from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Label
+from django.urls import reverse
+import task_manager.custom_test_objects as CO
+
+User = get_user_model()
+
 
 # Create your tests here.
+class StatusesTestCase(CO.CustomTestCase):
+    """
+    List, Create and Update tests are
+    inherited from CO.CustomTestCase
+    """
+    fixtures = ['labels/labels.json', 'users.json']
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+        self.model = Label
+        self.object1 = self.model.objects.get(pk=1)
+        self.object2 = self.model.objects.get(pk=2)
+        self.object3 = self.model.objects.get(pk=3)
+        self.object_list = [self.object1, self.object2, self.object3]
+        self.context_name = 'labels'
+        self.redirect_url = reverse('label_list')
+        self.create_url = reverse('label_create')
+        self.data_create = {
+            'name': 'tlabel',
+        }
+        self.update_url = reverse('label_update', kwargs={'pk': self.object2.id})
+        self.data_update = {
+            'name': 'tname',
+        }
+
+    def test_label_delete(self):
+        self.client.force_login(self.user)
+        url = reverse('label_delete', kwargs={'pk': self.object2.id})
+        response = self.client.post(url, follow=True)
+        self.assertEqual(self.model.objects.filter(id=self.object2.id).count(), 0)
+        self.assertRedirects(response, self.redirect_url)
