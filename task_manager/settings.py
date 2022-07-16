f7102627fbf7317fbd7473485922c5c684dfbca0
Natalia Teplynina <nt@mnt9.com>
2022-07-16 13:53:33 +0300

    change debug setting

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.getenv('DEBUG', '') != 'False'
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = [
     'webserver',
