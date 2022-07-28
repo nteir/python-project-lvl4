624be802e8ac9148d54f252171024974edbe3be0
Natalia Teplynina <nt@mnt9.com>
2022-07-28 09:40:49 +0300

    repeat myself on labels

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -1,24 +1,7 @@
 from django.urls import path
 from task_manager.labels import views
-from task_manager.statuses import views as statuses_views
+from task_manager.custom_objects import get_path_arguments
 
 app_name = 'labels'
-
-urlpatterns = [
-    path('', statuses_views.ObjectListView.as_view(**views.list_view_attr), name='obj_list'),
-    path(
-        'create/',
-        statuses_views.ObjectCreateView.as_view(**views.create_view_attr),
-        name='obj_create'
-    ),
-    path(
-        '<int:pk>/update/',
-        statuses_views.ObjectUpdateView.as_view(**views.update_view_attr),
-        name='obj_update'
-    ),
-    path(
-        '<int:pk>/delete/',
-        statuses_views.ObjectDeleteView.as_view(**views.delete_view_attr),
-        name='obj_delete'
-    ),
-]
+arguments = get_path_arguments(views)
+urlpatterns = [path(*a, **k) for a, k in arguments]
--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,10 +1,10 @@
 from .models import Label
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .forms import LabelCreateForm
 import task_manager.text_constants as txt
+import task_manager.custom_objects as CO
 
-# Re-using the List, Create, Update and Delete
-# views from statuses, passing new attributes in as_view()
 # Common attributes for Create and Update Views
 common_attr = {
     'model': Label,
@@ -15,34 +15,34 @@ common_attr = {
     'error_message': txt.NOT_LOGGED_IN,
 }
 
-list_view_attr = {
-    'model': Label,
-    'app_name': 'labels',
-    'title': txt.LABEL_LIST_TITLE,
-    'new_obj_text': txt.LABEL_LIST_NEW,
-}
-
-create_view_attr = {
-    'success_message': txt.CREATE_LABEL_SUCSESS,
-    'title_text': txt.CREATE_LABEL_TITLE,
-    'btn_text': txt.CREATE_BTN,
-}
-create_view_attr.update(common_attr)
-
-update_view_attr = {
-    'success_message': txt.UPDATE_LABEL_SUCSESS,
-    'title_text': txt.UPDATE_LABEL_TITLE,
-    'btn_text': txt.UPDATE_BTN,
-}
-update_view_attr.update(common_attr)
-
-delete_view_attr = {
-    'model': Label,
-    'success_url': reverse_lazy('labels:obj_list'),
-    'success_message': txt.DELETE_LABEL_SUCSESS,
-    'title_text': txt.DELETE_LABEL_TITLE,
-    'in_use_text': txt.LABEL_IN_USE,
-}
-
 
 # Create your views here.
+class ObjectListView(CO.CustomListView):
+
+    model = Label
+    app_name = 'labels'
+    title = txt.LABEL_LIST_TITLE
+    new_obj_text = txt.LABEL_LIST_NEW
+
+
+class ObjectCreateView(CO.CustomEditView, CreateView):
+
+    success_message = txt.CREATE_LABEL_SUCSESS
+    title_text = txt.CREATE_LABEL_TITLE
+    btn_text = txt.CREATE_BTN
+
+
+class ObjectUpdateView(CO.CustomEditView, UpdateView):
+
+    success_message = txt.UPDATE_LABEL_SUCSESS
+    title_text = txt.UPDATE_LABEL_TITLE
+    btn_text = txt.UPDATE_BTN
+
+
+class ObjectDeleteView(CO.CustomDeleteView):
+
+    model = Label
+    success_url = reverse_lazy('labels:obj_list')
+    success_message = txt.DELETE_LABEL_SUCSESS
+    title_text = txt.DELETE_LABEL_TITLE
+    in_use_text = txt.LABEL_IN_USE
