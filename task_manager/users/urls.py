e1714c28081145f4cbe2498a925ffbb92d417a84
Natalia Teplynina <nt@mnt9.com>
2022-07-28 10:14:12 +0300

    fix users url bug

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -5,14 +5,14 @@ from task_manager.users import views
 app_name = 'users'
 urlpatterns = [
     path('', views.UsersView.as_view(), name='users'),
-    path('users/create/', views.UserCreateView.as_view(), name='signup'),
+    path('create/', views.UserCreateView.as_view(), name='signup'),
     path(
-        'users/<int:pk>/update/',
+        '<int:pk>/update/',
         views.UserUpdateView.as_view(),
         name='update'
     ),
     path(
-        'users/<int:pk>/delete/',
+        '<int:pk>/delete/',
         views.UserDeleteView.as_view(),
         name='delete'
     ),
