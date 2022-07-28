33a584ecbb2802aa425dc581b05b9e5512a6ea6e
Natalia Teplynina <nt@mnt9.com>
2022-07-28 11:42:46 +0300

    fix long lines

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -5,6 +5,10 @@ app_name = 'labels'
 urlpatterns = [
     path('', views.ObjectListView.as_view(), name='obj_list'),
     path('create/', views.ObjectCreateView.as_view(**views.common_attr), name='obj_create'),
-    path('<int:pk>/update/', views.ObjectUpdateView.as_view(**views.common_attr), name='obj_update'),
+    path(
+        '<int:pk>/update/',
+        views.ObjectUpdateView.as_view(**views.common_attr),
+        name='obj_update'
+    ),
     path('<int:pk>/delete/', views.ObjectDeleteView.as_view(), name='obj_delete'),
 ]
--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -5,7 +5,11 @@ app_name = 'tasks'
 urlpatterns = [
     path('', views.ObjectListView.as_view(), name='obj_list'),
     path('create/', views.ObjectCreateView.as_view(**views.common_attr), name='obj_create'),
-    path('<int:pk>/update/', views.ObjectUpdateView.as_view(**views.common_attr), name='obj_update'),
+    path(
+        '<int:pk>/update/',
+        views.ObjectUpdateView.as_view(**views.common_attr),
+        name='obj_update'
+    ),
     path('<int:pk>/delete/', views.ObjectDeleteView.as_view(), name='obj_delete'),
     path('<int:pk>/', views.ObjectDetailView.as_view(), name='obj_card'),
 ]
